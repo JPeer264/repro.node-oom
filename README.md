48cbcddcab55ef9e6ac8280d0f511569269ebe46
JPeer264 <jan.peer@sentry.io>
2025-11-06 16:22:25 +0100

    feat: 💥💥

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # instructions
 
-- start container: `docker-compose up --build`
-- run stresstest: `npx loadtest -n 10 -c 5 http://localhost:3000/api/data` (-n for max requests, -c for concurrency)
+1. `pnpm i`
+2. start container: `pnpm dev`
+3. check Memory Consumption in DevTools
+4. open: http://localhost:3000/api/data in the browser
+
+---
+
+1. comment / remove `integrations` in `instrument.js`
+2. run again everything from above again
+3. compare memory consumption & console output
--- instrument.js
+++ instrument.js
@@ -1,13 +1,16 @@
+// @ts-check
 const Sentry = require("@sentry/node");
 
 // Ensure to call this before importing any other modules!
 Sentry.init({
-  dsn: "https://22dafb110691e9326c3e282ef6fe841e@o447951.ingest.us.sentry.io/4509513526542336",
+  dsn: "https://68b15e200f59b55032af16edc2a495d2@o447951.ingest.us.sentry.io/4510141899079680",
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/node/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
   _experiments: {
     enableMetrics: true,
   },
-  // debug: true,
+  integrations: [Sentry.asyncIntegration()],
+  defaultIntegrations: false,
+  debug: true,
 });
